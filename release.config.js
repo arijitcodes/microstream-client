72890b72dee0f3551cc70a2f16b0946712eb5a67
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2025-03-01 04:08:24 +0530

    feat(semantic-release): set up Semantic-Release for auto Release & Update
        Setup Semantic-Release with Commitizen - along with GitHub Workflow. Now this setup will -
    
         - Auto Release on GitHub, NPM
         - Update Changelog automatically from Conventional Commit messages and update version in package.json
         - Set up Commitizen for Conventional Commit message formatting
         - Setup GitHub Workflow to automate all these tasks
    
    fix #1

---
+++ release.config.js
@@ -0,0 +1,48 @@
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
+module.exports = {
+  branches: ["main"],
+  // repositoryUrl: "git@github.com:arijitcodes/microstream-hub.git", // Keep this only for local `npx semantic-release --dry-run` NOT FOR CI/CD ON GITHUB WORKFLOW - COmment it
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        writerOpts: {
+          commitPartial: readFileSync(
+            join(__dirname, "./configs/semantic-release/commit-template.hbs"),
+            "utf-8"
+          ),
+        },
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          {
+            path: "microstream-client.tar.gz", // Adjust based on your build output
+            name: "microstream-client_v${nextRelease.version}.tar.gz",
+            label: "MicroStream Client SDK v${nextRelease.version}",
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package*.json", "bun.lockb"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/npm", // Add this for the Client SDK only
+  ],
+};
